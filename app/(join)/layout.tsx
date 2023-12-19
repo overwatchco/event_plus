903dc233fc65fb906a8317e457afbd60736f2046
Joan Sebastián Giraldo Morales <36310454+SebastianJoan@users.noreply.github.com>
2023-12-19 13:41:57 -0500

    upload

--- app/(join)/layout.tsx
+++ app/(join)/layout.tsx
@@ -1,6 +1,3 @@
-import { redirect } from "next/navigation";
-import { authOptions } from "../api/auth/[...nextauth]/route";
-import { getServerSession } from "next-auth";
 
 export default async function JoinLayout( {children} : { children: React.ReactNode }) {  
   return (
