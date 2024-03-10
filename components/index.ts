39c31ca9329e75d4882055b136f47d67237a4346
Kerpooo <kevinrincon8@gmail.com>
2024-03-10 00:31:06 -0500

    chore: :fire:

--- components/index.ts
+++
@@ -1,6 +0,0 @@
-export { Navbar } from './navbar/LandigNavbar'
-export { MainNavbar } from './navbar/MainNavbar'
-
-
-export { BasicDashboardCard } from './cards/BasicDashboardCard'
-export { UsersDashboardCard } from './cards/UsersDashboardCard'
