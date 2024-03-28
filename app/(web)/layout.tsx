b850d0b6336f17c8f75d707f6de1bb1cfed859b3
Kerpooo <kevinrincon8@gmail.com>
2024-03-27 21:49:33 -0500

    feat(mainLayout): :lipstick: Se añade la funcionalidad del toast

--- app/(web)/layout.tsx
+++ app/(web)/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider"
 import "../globals.css";
 import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/toaster"
 
 export const fontSans = FontSans({
   subsets: ["latin"],
@@ -20,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={cn(
         "min-h-screen bg-background font-sans antialiased",
         fontSans.variable
@@ -32,6 +33,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
