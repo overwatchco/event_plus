2c3cbab71831cea42cebb2213e560377bbf70b19
Kerpooo <kevinrincon8@gmail.com>
2024-04-01 15:04:48 -0500

    fix(eventos): :bug: getEvento tipo
    Se añade que cuando se obtenga un evento mediante su ID este tambien puede ser null en caso de que no se encuentre
    
    }

--- actions/evento-actions.ts
+++ actions/evento-actions.ts
@@ -41,64 +41,6 @@ export const addEvento = async (
 }
 
 
-
-// // Actualizar empresa
-// export const updateEmpresa = async (
-//     idToUpdate: string,
-//     data: {
-//         nit: string,
-//         nombre: string,
-//         pais: string,
-//         departamento: string,
-//         ciudad: string
-//         direccion: string,
-//         telefono: number,
-//     }
-// ) => {
-//     const empresa = await prisma.empresa.findFirst({ where: { id: idToUpdate } });
-
-//     if (!empresa) {
-//         throw `Empresa con id ${idToUpdate} no encontrado`
-//     }
-
-//     const updatedEmpresa = await prisma.empresa.update({
-//         where: { id: idToUpdate },
-//         data
-//     });
-
-
-//     revalidatePath('/empresas')
-//     return updatedEmpresa
-// };
-
-
-
-
-
-// // // Obtener empresa
-// // export const getEmpresa = async (id: string): Promise<Empresa> => {
-
-// //     const empresa = await prisma.empresa.findFirst({ where: { id } })
-
-// //     if (!empresa) {
-// //         throw `Empresa con id ${id} no encontrado`
-// //     }
-
-// //     return empresa
-// // }
-
-
-// // //Eliminar empresa
-
-// // export const deleteEmpresa = async (id: string): Promise<void> => {
-
-
-// //     await prisma.empresa.delete({ where: { id } })
-// //     revalidatePath('/empresas')
-
-// // }
-
-
 export const getEventos = async (): Promise<Evento[]> => {
     try {
         const contratos = await getListContratos();
@@ -118,8 +60,7 @@ export const getEventos = async (): Promise<Evento[]> => {
 }
 
 
-//TODO: Arreglar el tipo que devuelve esta funcion
-export const getEvento = async (eventoId: string): Promise<any> => {
+export const getEvento = async (eventoId: string): Promise<Evento | null> => {
     try {
         const evento = await prisma.evento.findUnique({
             where: {
